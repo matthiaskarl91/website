0ac201d76892253f2a04838d4cdea9321060aa1d
Matthias <matthias.karl@cmdscale.com>
2023-07-25 11:14:52 +0200

    feat: add index page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,15 @@
 import { defineConfig } from 'astro/config';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
+import mdx from '@astrojs/mdx';
 
-import mdx from "@astrojs/mdx";
+import vercel from '@astrojs/vercel/serverless';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), mdx()]
+	experimental: {
+		assets: true,
+	},
+	integrations: [tailwind(), mdx()],
+	output: 'server',
+	adapter: vercel(),
 });
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,17 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
-  darkMode: "class",
-  theme: {
-    extend: {
-      colors: {
-        delftBlue: "#29335C",
-        flame: "#E4572E",
-        orangeWeb: "#F3A712",
-        olivine: "#A8C686",
-        airBlue: "#669BBC",
-      },
-    },
-  },
-  plugins: [require("@tailwindcss/typography")],
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	darkMode: 'class',
+	theme: {
+		extend: {
+			colors: {
+				delftBlue: '#29335C',
+				flame: '#E4572E',
+				orangeWeb: '#F3A712',
+				olivine: '#A8C686',
+				airBlue: '#669BBC',
+			},
+			fontFamily: {
+				sans: ['Fira Mono', ...defaultTheme.fontFamily.sans],
+			},
+		},
+	},
+	plugins: [require('@tailwindcss/typography')],
 };
